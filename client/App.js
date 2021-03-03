366331bce6034369800a625b21aa6d02391b7eea
shahprose <72280598+shahprose@users.noreply.github.com>
2021-03-03 00:16:14 -0500

    codemirror

--- client/App.js
+++ client/App.js
@@ -1,34 +1,30 @@
 import React, { useContext, createContext, useEffect } from 'react';
-import { io } from "socket.io-client";
-import LoginContainer from "./containers/LoginContainer"
-import WaitingRoomContainer from "./containers/WaitingRoomContainer"
-import GameContainer from "./containers/GameContainer"
+import { io } from 'socket.io-client';
+import LoginContainer from './containers/LoginContainer';
+import WaitingRoomContainer from './containers/WaitingRoomContainer';
+import GameContainer from './containers/GameContainer';
 const socket = io();
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
+  const checkSocket = () => {
+    socket.emit('test', `This is the first socket test from ${socket.id}`);
+  };
 
-const checkSocket = () => {
-   socket.emit('test', `This is the first socket test from ${socket.id}`)
-}
+  return (
+    // <div>
+    //     <button onClick={checkSocket}>click</button>
+    // </div>
 
-    return (
-        // <div>
-        //     <button onClick={checkSocket}>click</button>
-        // </div> 
-
-        <Router>
-            <Switch>
-                <Route path='/waitingroom' component={WaitingRoomContainer} />
-                {/* <Route exact path='/gameContainer' component={GameContainer} /> */}
-                <Route exact path='/' component={LoginContainer}/>
-            </Switch>
-        </Router>
-
-        
-    )
-}
+    <Router>
+      <Switch>
+        <Route path='/waitingroom' component={WaitingRoomContainer} />
+        <Route exact path='/game' component={GameContainer} />
+        <Route exact path='/' component={LoginContainer} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
--- client/components/Codemirror.js
+++ client/components/Codemirror.js
@@ -1,36 +1,46 @@
 import React from 'react';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
 
 // import context object
 // import { CodeContext } from '../state/contexts';
 
 function CodeBox({ codeBoxName }) {
-//   const { codeState } = useContext(CodeContext);
+  //   const { codeState } = useContext(CodeContext);
 
-//   let code;
-//   codeState.showSchema
-//     ? (code = codeState.schema)
-//     : (code = codeState.resolver);
+  //   let code;
+  //   codeState.showSchema
+  //     ? (code = codeState.schema)
+  //     : (code = codeState.resolver);
+
+  function handleChange(editor, data, value) {
+    console.log(value);
+  }
 
   return (
     <div className='codebox'>
-      <CodeMirror 
-       value={codeBoxName}
-       options={{
-        mode: 'javascript',
-        lineWrapping: true,
-        theme: 'dracula',
-        lineNumbers: true,
-        cursorScrollMargin: 48,
-        indentUnit: 2,
-        tabSize: 2,
-        styleActiveLine: true,
-        smartIndent: true,
-       }}
-       />
+      <CodeMirror
+        onChange={handleChange}
+        className='code-mirror-container'
+        value={codeBoxName}
+        options={{
+          mode: 'javascript',
+          lineWrapping: true,
+          theme: 'dracula',
+          lineNumbers: true,
+          cursorScrollMargin: 48,
+          indentUnit: 2,
+          tabSize: 2,
+          styleActiveLine: true,
+          smartIndent: true,
+        }}
+      />
     </div>
-  )
+  );
 }
 
 export default CodeBox;
