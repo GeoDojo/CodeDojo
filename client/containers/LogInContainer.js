14dbd520995e5814533ab02d17a53f37ee5203d8
shahprose <72280598+shahprose@users.noreply.github.com>
2021-03-03 21:22:56 -0500

    logo

--- client/containers/LogInContainer.js
+++ client/containers/LogInContainer.js
@@ -2,12 +2,11 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import LoginButton from '../components/Button';
 import { io } from 'socket.io-client';
-const socket = io()
+const socket = io();
 
 import { AppContext } from '../state/context';
 
 const LogInContainer = () => {
-
   const { appState } = useContext(AppContext);
   //to-do: using OAuth instructions
   // const googleLogin = () => {
@@ -20,33 +19,39 @@ const LogInContainer = () => {
 
   return (
     <div className='login-container'>
-      <Link to='/game'>
-        {/* Reusing the Button.js component and passing genericClick to  */}
-        <LoginButton
-          id='GoogleBtn'
-          siteName={'Google'}
-          genericClick={'googleLogin'}
-        />
-        <LoginButton
-          id='FacebookBtn'
-          siteName={'Facebook'}
-          genericClick={'FACEBOOK Login'}
-        />
-        <LoginButton
-          id='GithubBtn'
-          siteName={'GitHub'}
-          genericClick={'GITHUB Login'}
-        />
-      
-        <LoginButton
-          id='AnonymousBtn'
-          siteName={'Anonymous'}
-          genericClick={() => {
-            console.log('room # in loginContainer: ', appState.roomNumber)
-            socket.emit('joinRoom', appState.roomNumber)
-          }}
-        />
-      </Link> 
+      <div className='banner-container'>
+        <div className=''>CODE</div>
+        <div className=''>DOJO</div>
+      </div>
+      <div className='login-container'>
+        <Link to='/game'>
+          {/* Reusing the Button.js component and passing genericClick to  */}
+          <LoginButton
+            id='GoogleBtn'
+            siteName={'Google'}
+            genericClick={'googleLogin'}
+          />
+          <LoginButton
+            id='FacebookBtn'
+            siteName={'Facebook'}
+            genericClick={'FACEBOOK Login'}
+          />
+          <LoginButton
+            id='GithubBtn'
+            siteName={'GitHub'}
+            genericClick={'GITHUB Login'}
+          />
+
+          <LoginButton
+            id='AnonymousBtn'
+            siteName={'Anonymous'}
+            genericClick={() => {
+              console.log('room # in loginContainer: ', appState.roomNumber);
+              socket.emit('joinRoom', appState.roomNumber);
+            }}
+          />
+        </Link>
+      </div>
     </div>
   );
 };
